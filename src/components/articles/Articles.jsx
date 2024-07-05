0063419b9c054d6e01bc3df49ffbddcb8d36168c
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-07-05 22:03:44 +0300

    adding caroussel container

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from "../Header/Header"
+import Caroussel from '../caroussel/Caroussel';
+
+
+// import images from '../../../public/images.jpeg'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Articles = () => {
     return (
         <div>
             <Header />
+            <div className="caroussel-container">
+                <Caroussel />
+            </div>
         </div>
     );
 }
