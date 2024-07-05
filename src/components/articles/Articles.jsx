0bb0c84ad68ac75fe4d14c4123609e82677ac940
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-07-05 22:19:26 +0300

    filter and search bar added

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from "../Header/Header"
 import Caroussel from '../caroussel/Caroussel';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 
 // import images from '../../../public/images.jpeg'
@@ -12,6 +14,26 @@ const Articles = () => {
             <Header />
             <div className="caroussel-container">
                 <Caroussel />
+            </div>
+
+            <div className="container mt-4">
+                <h3 className='text-center text-success'>Listes Des Articles</h3>
+                <div className="row">
+                    <div className="col-1 mx-2"><button className='btn btn-success'>Rechercher</button></div>
+                    <div className="col-4">
+                     <input type="text" className="form-control" placeholder='trouver un article specifique ici...' />
+                    </div>
+                    <div className="col-6">
+                            <ListGroup horizontal>
+                      
+                            <ListGroup.Item id='active-filtre'>Tout</ListGroup.Item>
+                            <ListGroup.Item>Vetements</ListGroup.Item>
+                            <ListGroup.Item>Chaussures</ListGroup.Item>
+                            <ListGroup.Item>Informatiques</ListGroup.Item>
+                            </ListGroup>
+                    </div>
+                </div>
+               
             </div>
         </div>
     );
